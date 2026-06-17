81338d15940534744e9040ecb9616114b3ea0778
Mordy Pesach <mordypassover@gmail.com>
2026-06-17 17:20:44 +0300

    add update met

--- database/agent_db.py
+++ database/agent_db.py
@@ -35,6 +35,21 @@ class AgentDB:
         conn.close()
         return agent
 
+    def update_agent(self, id, data):
+        if "id" in data.keys():
+            raise KeyError("cang update id")
+        keys = [f"{k}=%s" for k in data.keys()]
+        key_string = ", ".join(keys)
+        conn =self.conn()
+        cursor = conn.cursor()
+        query = f"UPDATE agent_db SET {key_string} WHERE id =%s"
+        cursor.execute(query, [data[k] for k in data.keys()]+[id])
+        is_updated = cursor.rowcount >0
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return is_updated
+
 
 
 if __name__ == "__main__":
@@ -42,3 +57,4 @@ if __name__ == "__main__":
     #print(a.create_agent({"name":"mordy", "specialty":"qwer","agent_rank":"senior"}))
     print(a.get_all_agents())
     print(a.get_agent_by_id(1))
+    # print(a.update_agent(1,{"specialty":"wer"} ))
