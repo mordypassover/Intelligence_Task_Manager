2fc8b5074b7bde8ecb7d4ca5f563465559660d33
Mordy Pesach <mordypassover@gmail.com>
2026-06-17 15:54:53 +0300

    reorder query

--- database/db_connection.py
+++ database/db_connection.py
@@ -33,7 +33,18 @@ class DBConnection:
     def create_tables(self):
         conn = self.get_connection()
         cursor = conn.cursor()
-        query = "CREATE TABLE IF NOT EXISTS agent_db (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL, specialty VARCHAR(50) NOT NULL, is_active BOOLEAN DEFAULT TRUE, completed_missions INT DEFAULT 0, failed_missions INT DEFAULT 0); CREATE TABLE IF NOT EXISTS missions_db (id INT PRIMARY KEY AUTO_INCREMENT ,name VARCHAR(50) NOT NULL, description TEXT NOT NULL, location VARCHAR(50) NOT NULL, difficulty INT CHECK(difficulty < 11 AND difficulty > 0), importance INT CHECK(importance < 11 AND importance > 0))"
+        query = ("CREATE TABLE IF NOT EXISTS agent_db (id INT PRIMARY KEY AUTO_INCREMENT,"
+                 " name VARCHAR(50) NOT NULL,"
+                 " specialty VARCHAR(50) NOT NULL,"
+                 " is_active BOOLEAN DEFAULT TRUE,"
+                 " completed_missions INT DEFAULT 0,"
+                 " failed_missions INT DEFAULT 0);"
+                 " CREATE TABLE IF NOT EXISTS missions_db (id INT PRIMARY KEY AUTO_INCREMENT"
+                 " ,name VARCHAR(50) NOT NULL,"
+                 " description TEXT NOT NULL,"
+                 " location VARCHAR(50) NOT NULL,"
+                 " difficulty INT CHECK(difficulty < 11 AND difficulty > 0),"
+                 " importance INT CHECK(importance < 11 AND importance > 0))")
         for i in query:
             cursor.execute(i)
             conn.commit()
